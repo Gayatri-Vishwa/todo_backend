957001f7fb8a19a027807007328c3a83465c76aa
gayatriDev02 <blossomjasmine02@gmail.com>
2026-02-13 09:10:24 +0530

    first commit

--- connection/conn.js
+++ connection/conn.js
@@ -1,9 +1,13 @@
 
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // load .env variables
+
 
 const conn= async ()=>{ //use async await or .then  is compulsory
     try{                                       
-    await mongoose.connect("mongodb+srv://user:@cluster0.khydod6.mongodb.net/usersdata?retryWrites=true&w=majority")  // connection string  copy from mongodb atlas  (username password apna) usersdata is the database name — MongoDB will create it automatically if it doesn’t exist.
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.khydod6.mongodb.net/usersdata?retryWrites=true&w=majority`)  // connection string  copy from mongodb atlas  (username password apna) usersdata is the database name — MongoDB will create it automatically if it doesn’t exist.
         console.log("Connected to MongoDB");
    }
     catch(err)  {
