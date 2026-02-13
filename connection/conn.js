49ec61d5505f284af60c3f78b13110d29ec9b2a6
gayatriDev02 <blossomjasmine02@gmail.com>
2026-02-13 09:05:38 +0530

    first commit

--- connection/conn.js
+++ connection/conn.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const conn= async ()=>{ //use async await or .then  is compulsory
     try{                                       
-    await mongoose.connect("mongodb+srv://user:gayatri02%40@cluster0.khydod6.mongodb.net/usersdata?retryWrites=true&w=majority")  // connection string  copy from mongodb atlas  (username password apna) usersdata is the database name — MongoDB will create it automatically if it doesn’t exist.
+    await mongoose.connect("mongodb+srv://user:@cluster0.khydod6.mongodb.net/usersdata?retryWrites=true&w=majority")  // connection string  copy from mongodb atlas  (username password apna) usersdata is the database name — MongoDB will create it automatically if it doesn’t exist.
         console.log("Connected to MongoDB");
    }
     catch(err)  {
